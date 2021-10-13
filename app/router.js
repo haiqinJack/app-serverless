573b264461655111fe55ecbd758a4e36672e327e
haiqinJack <1198617472@qq.com>
2021-10-13 22:03:09 +0800

    remove router.login

--- app/router.js
+++ app/router.js
@@ -1,19 +1,14 @@
 'use strict';
-const LocalStrategy = require('passport-local').Strategy;
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
   const { router, controller, config: { apiPrefix } } = app;
-  const localStrategy = app.passport.authenticate('local');
   
   // router.get('/passport', deserializeUser)
   router.get('/', 'home.index');
   router.get('/logout', 'home.logout');
   router.get('/admin', controller.admin.index)
-  router.post('/passport/local', localStrategy);
-  // 登录校验
-  // router.post('/login', app.passport.authenticate('local', { successRedirect: '/authCallback' }));
   
   router.resources('users', '/users', controller.users);
 };
